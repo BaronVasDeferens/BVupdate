e0380aaae4863a57f641eff06abdff5101b3a101
skot <baronvasdeferens@gmail.com>
2017-07-06 22:39:21 -0700

    Init

---
+++ src/bvupdate/BVupdate.java
@@ -0,0 +1,68 @@
+/*
+ *
+Map Updater
+Allows for easy-update re-drawing of the ever-changing Bunns Village
+availabilities map.
+
+USAGE: This tool is designed to take as arguments all AVAILABLE units in the
+following format: mapupdate 11 13 21A 17B
+
+NOTE: "3A 3B" will produce different results than just "3"
+
+All buildings NOT specified are considered to be OCCUPIED
+
+The MapUpdater is up first, generating the lists of occupied and available units.
+The WebUpdater uses the data furnished by MapUpdater to generate a new website
+
+OUTPUT: index.html and map.png
+
+TODO: generate a new & updated version of each individual building web page
+TODO: load, modify, and save a new version of the application web page using the
+      // updated data to populate the drop-down box
+
+ */
+
+package bvupdate;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class BVupdate {
+
+
+    public static void main(String... args) {
+
+        // Check for arguments:
+        // If there were none, assume all building are set to occupied
+        if (args.length == 0) {
+            System.out.println("MAP UPDATER: ");
+            System.out.println("Setting ALL BUILDINGS TO OCCUPIED");
+        }
+
+
+        Connection connection = null;
+
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:buildings.db");
+            System.out.println("Database load success...");
+
+            // Update the map and website
+            // TODO: also create individual website for building
+            // TODO: update application drop-down box to reflect availabilities
+            MapUpdater mapupdater = new MapUpdater(connection, args);
+            WebUpdater webupdater = new WebUpdater(mapupdater);
+
+            connection.close();
+
+        } catch (Exception e) {
+            throw new RuntimeException(e.toString());
+        } finally {
+
+        }
+
+
+    }
+
+}
+
---
+++ src/bvupdate/MapUpdater.java
@@ -0,0 +1,285 @@
+package bvupdate;/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.awt.AlphaComposite;
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Polygon;
+import java.awt.image.BufferedImage;
+import java.awt.image.RenderedImage;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import javax.imageio.ImageIO;
+import java.sql.*;
+import java.sql.SQLException;
+
+
+/**
+ *
+ * @author skot
+ */
+public class MapUpdater {
+    
+    BufferedImage map = null;
+    HashMap occupied;
+    HashMap avails;
+    Connection connection;
+    
+    // UPDATER CONSTRUCTOR
+    
+    public MapUpdater(Connection connection, String ... args) {
+        
+        this.connection = connection;
+        
+        occupied = new <Integer, Building>HashMap();
+        avails = new <Integer, Building>HashMap();
+        
+        populateHashmap(args);
+        sortBuildings(args);
+        
+        map = loadMapImage();
+        drawBuildings();
+        saveMap();
+        
+    }
+    
+    // CREATE BUILDINGS
+    // Takes the ResultSet from the database query; 
+    // populates hashmap "occupied" using the data from resultset
+    private void createBuildings(ResultSet results) {
+        
+        try {
+            while (results.next()) {
+                String number, address, coords, ohdDescription, notes, temp;
+                int length, width, height, sqfeet, mandoors, restrooms, restrooms_ada, rate;
+                Boolean threePhase = false, hasAC = false;
+                Polygon poly;
+                Building b;
+
+                // Grab values from the results
+                number = results.getString("number");
+                address = results.getString("address");
+                coords = results.getString("coords");
+                length = results.getInt("length");
+                width = results.getInt("width");
+                height = results.getInt("height");
+                sqfeet = length * width;
+                mandoors = results.getInt("mandoors");
+                ohdDescription = results.getString("ohd_desc");
+                restrooms = results.getInt("restrooms");
+                restrooms_ada = results.getInt("restrooms_ada");
+                rate = results.getInt("rate");
+                notes = results.getString("notes");
+                
+                temp = results.getString("three_phase");
+                if (temp != null)
+                    if (temp.matches("yes"))
+                       threePhase = true;
+                
+                temp = results.getString("ac");
+                if (temp != null)
+                    if (temp.matches("yes"))
+                       hasAC = true;
+
+                
+                poly = new Polygon();
+                poly.reset();
+                
+                if (coords != null) {
+                // Extract coordinates from "coords"
+                    String coordsArray[] = coords.split(",", 8);
+                    for (int i = 0; i < 8; i+= 2) {
+                        poly.addPoint(Integer.parseInt(coordsArray[i]), Integer.parseInt(coordsArray[i+1]));
+                    }
+                }
+               
+                // Construct new building objkect from db results
+                // and store it in the hash, indexed by it's unique "number" field
+                b = new Building(number,address, poly);
+                b.setDoors(mandoors, ohdDescription);
+                b.setOptions(threePhase, hasAC);
+                b.setRate(rate);
+                b.setRestrooms(restrooms, restrooms_ada);
+                b.setSize(length, width, height);
+                
+                // Chunk up the "notes" and add them as Building.features.
+                // Delineate by semicolon (up to max of 10 feature strings)
+                if (notes != null) {
+                    String featureArray[] = notes.split(";",10);
+                    for (int i = 0; i < featureArray.length; i++) {
+                        b.addFeature(featureArray[i]);
+                    }
+                }
+                
+                occupied.put(number.hashCode(), b);
+
+            }
+        }
+        
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+    }
+    
+    // POPULATE HASHMAP
+    // Creates a total listing of the buildings, their attributes, and corresponding 
+    //positions within the image.
+    private void populateHashmap(String ... args) {
+       
+        Polygon poly;
+        Building b;
+        String queryString = "SELECT * FROM buildings";
+        PreparedStatement statement = null;
+        ResultSet results = null;
+
+        
+        try {
+
+            statement = connection.prepareStatement(queryString);
+            results = statement.executeQuery();
+            
+            if (results != null)
+                createBuildings(results);
+            
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        finally {
+            
+            if (statement != null) {
+                try {
+                    statement.close();
+                }
+                catch (SQLException e) {
+                    System.out.println(e.toString());
+                }
+            }
+            
+            if (results != null) {
+                try {
+                    results.close();
+                }
+                catch (SQLException e) {
+                    System.out.println(e.toString());
+                }
+            }
+        }
+           
+    }
+    
+    
+    // LOAD MAP IMAGE
+    // Loads the "blank" map
+    private BufferedImage loadMapImage() {
+        
+        BufferedImage return_image = null;
+       
+        try (InputStream fin = getClass().getResourceAsStream("resources/blankmap.png");){
+            return_image = ImageIO.read(fin);
+        }
+        catch (IOException e)
+        {
+            System.out.println(e.toString());
+            System.exit(1);
+        }
+        
+        return(return_image);
+    }
+    
+    
+    // SORT BUILDINGS
+    // Separates the occupied buildings and available buildings into two distinct
+    // hashmaps.
+    private void sortBuildings(String ... args) {
+        
+        // Remove all of the "occupied" buildings from the occupied map
+        // and store them in a seperate hash in order to later generate
+        // a clickmap for the availabilities
+        
+        for (String bldg: args) {
+            
+
+            System.out.print("removing " + bldg);
+            
+            if (!occupied.containsKey(bldg.hashCode()))
+                System.out.print("...NOT FOUND!\n");
+            else {
+                avails.put(bldg.hashCode(), occupied.get(bldg.hashCode()));
+                occupied.remove(bldg.hashCode());
+                System.out.print("...OK!\n");
+            }
+            
+            // In the event that user input is something like "3A 3B", the entry for "3"
+            // should be scrubbed
+            if (bldg.endsWith("A") || (bldg.endsWith("B"))) { 
+                
+                bldg = bldg.substring(0, (bldg.length() - 1));
+                System.out.print("...also removing " + bldg);
+                
+                if (occupied.containsKey(bldg.hashCode())) {
+                    occupied.remove(bldg.hashCode());
+                    System.out.println("...OK!");
+                }
+                else
+                    System.out.println("...NOT FOUND!");
+            }
+        }
+        
+    }
+    
+    // DRAW BUILDINGS
+    // Accepts the list of available buildings and draws them to the map
+    private void drawBuildings() {
+        
+        // Setup the graphics context
+        Graphics2D g2 = map.createGraphics();
+        g2.setComposite(AlphaComposite.SrcOver.derive(0.8f));
+        g2.setColor(new Color(180,80,0));
+        
+        // Begin drawing over the sequence of occupied buildings
+        Polygon p;
+        Set<Map.Entry<String, Building>> set = occupied.entrySet();
+
+        for (Map.Entry<String, Building> me: set) {
+            //System.out.println("drawing " + me.getKey());
+            p = (Polygon)me.getValue().myPoly;
+            g2.fillPolygon(p);
+        }     
+    }
+    
+    
+    // SAVE MAP
+    // Handles the out-to-disk duties, saving the new map as "map.png"
+    private void saveMap() {
+        
+        if (map == null) {
+            System.out.println("ERROR: saveMap(): map is null!");
+            return;
+        }
+        
+        RenderedImage renderedMap = (RenderedImage)map;
+        
+        try {
+            //OutputStream out = new FileOutputStream(new File("images/map.png"));
+            File out = new File("map.png");
+            ImageIO.write(renderedMap, "png", out);
+            System.out.println("SUCCESS!");
+        }
+        catch (IOException e) {
+            System.out.println("ERROR saveMap(): " + e.toString());
+        }
+        
+    }
+    
+    
+}
